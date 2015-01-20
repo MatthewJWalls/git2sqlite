a5b391c320840d21f824a45deeae45d0096136df
MatthewJWalls <codered1200@googlemail.com>
2015-01-20 07:59:04 +0000

    Progress on index file unpacking.

--- unpack_test.go
+++ unpack_test.go
@@ -14,7 +14,54 @@ func makeInteger(in int) []byte {
 	return bs
 }
 
+func TestIndexFileUnpack(t *testing.T) {
+
+	log.Println("Unpacking index.")
+
+	path := filepath.Join(
+		"test-data", 
+		"samplerepo", 
+		"objects", 
+		"pack", 
+		"pack-f15a2879b1e01aece417a8c1d06f5079858633f4.idx",
+	)
+
+	rawbytes, readerr := ioutil.ReadFile(path)
+			
+	if readerr != nil {
+		log.Fatal(readerr)
+	}
+
+	unpacker := Unpacker{raw:rawbytes}
+	_ = unpacker
+
+	log.Printf("index signature: %q", unpacker.Bytes(4))
+	log.Printf("index version: %d", unpacker.UInt(4))
+
+	for i := 0; i < 255; i += 1 {
+		log.Printf("%d fanout: %d", i, unpacker.UInt(4))
+	}
+
+	size := unpacker.UInt(4)
+
+	for i := 0; i < size; i += 1 {
+		log.Printf("%d entry: %x", i, unpacker.String(20))
+	}
+
+	for i := 0; i < size; i += 1 {
+		log.Printf("%d crc: %d", i, unpacker.UInt(4))
+	}
+
+	for i := 0; i < size; i += 1 {
+		val := unpacker.UInt(4)
+		log.Printf("%d offset: %d %b", i, val, val)
+	}
+
+}
+
 func TestPackFileUnpack(t *testing.T) {
+
+	log.Println("Unpacking pack file.")
 
 	path := filepath.Join(
 		"test-data", 
